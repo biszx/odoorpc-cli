08ffd57fb2163cd333bc80276d761529b1ef6db8
Isares Srirattanapaisarn <isares.br@gmail.com>
2026-05-09 11:51:51 +0700

    IMP: help and version option

--- odoocli/cli.py
+++ odoocli/cli.py
@@ -1,9 +1,11 @@
 import click
 
+from odoocli import __version__
 from odoocli.commands import auth, call_method, create, model, search, unlink, write
 
 
-@click.group()
+@click.group(context_settings={"help_option_names": ["-h", "--help"]})
+@click.version_option(version=__version__, prog_name="odoocli")
 def odoo():
     """Odoo CLI - interact with your Odoo instance from the command line"""
     pass
